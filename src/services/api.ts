9a21b87e12d90665ae2c6d3d39971185f403492c
unknown <lankoandeenock002@gmail.com>
2026-05-10 17:50:38 +0000

    serveur

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://alertesos.onrender.com';
 
 // Instance Axios principale avec intercepteur JWT
 export const api = axios.create({
